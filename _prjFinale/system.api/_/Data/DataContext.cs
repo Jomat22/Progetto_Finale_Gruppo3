29a2a7e1028b1c6120c0d66e5052ed0c56a8c8d0
UserGit-Dev <welcometorapture@hotmail.it>
2026-03-20 22:01:25 +0100

    5

--- _prjFinale/system.api/_/Data/DataContext.cs
+++ _prjFinale/system.api/_/Data/DataContext.cs
@@ -16,7 +16,7 @@ class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
             entity.ToTable("persone");
             entity.HasKey(pk => pk.Id);
           
-            entity.Property(e => e.Id).HasColumnOrder(1).HasColumnName("id").HasColumnType("int");
+            entity.Property(e => e.Id).HasColumnOrder(1).HasColumnName("id").HasColumnType("int").ValueGeneratedOnAdd().IsRequired();
             entity.Property(e => e.CodiceFiscale).HasColumnOrder(2).HasColumnName("codice_fiscale").HasColumnType("varchar(16)");
             entity.Property(e => e.Nome).HasColumnOrder(3).HasColumnName("nome").HasColumnType("varchar(100)");
             entity.Property(e => e.Cognome).HasColumnOrder(4).HasColumnName("cognome").HasColumnType("varchar(100)");
@@ -27,7 +27,7 @@ class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
             entity.Property(e => e.CodicePostale).HasColumnOrder(9).HasColumnName("codice_postale").HasColumnType("varchar(10)");
             entity.Property(e => e.Indirizzo).HasColumnOrder(10).HasColumnName("indirizzo").HasColumnType("varchar(255)");
             entity.Property(e => e.NumeroContatto).HasColumnOrder(11).HasColumnName("numero_contatto").HasColumnType("varchar(20)");
-            entity.Property(e => e.Email).HasColumnOrder(12).HasColumnName("email").HasColumnType("varchar(100)");
+            entity.Property(e => e.Email).HasColumnOrder(12).HasColumnName("email").HasColumnType("varchar(255)");
             entity.Property(e => e.IsDeleted).HasColumnOrder(13).HasColumnName("is_deleted").HasColumnType("tinyint(1)");
             entity.Property(e => e.CreatedAt).HasColumnOrder(14).HasColumnName("created_at").HasColumnType("datetime(6)");
             entity.Property(e => e.ModifiedAt).HasColumnOrder(15).HasColumnName("modified_at").HasColumnType("datetime(6)");
@@ -39,14 +39,14 @@ class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
             entity.HasKey(pk => pk.Id);
             entity.HasOne(np => np.Person).WithOne().HasForeignKey<Client>(fk => fk.PersonId);
 
-            entity.Property(e => e.Id).HasColumnOrder(1).HasColumnName("id").HasColumnType("int");
-            entity.Property(e => e.PersonId).HasColumnOrder(2).HasColumnName("codice_fiscale").HasColumnType("varchar(16)");
+            entity.Property(e => e.Id).HasColumnOrder(1).HasColumnName("id").HasColumnType("int").ValueGeneratedOnAdd().IsRequired();
+            entity.Property(e => e.PersonId).HasColumnOrder(2).HasColumnName("person_id").HasColumnType("int");
             entity.Property(e => e.CodiceCliente).HasColumnOrder(3).HasColumnName("codice_cliente").HasColumnType("varchar(20)");
             entity.Property(e => e.IsFidelizzato).HasColumnOrder(4).HasColumnName("is_fidelizzato").HasColumnType("tinyint(1)");
             entity.Property(e => e.IsIscrittoNewsletter).HasColumnOrder(5).HasColumnName("is_iscritto_newsletter").HasColumnType("tinyint(1)");
-            entity.Property(e => e.IsDeleted).HasColumnOrder(13).HasColumnName("is_deleted").HasColumnType("tinyint(1)");
-            entity.Property(e => e.CreatedAt).HasColumnOrder(14).HasColumnName("created_at").HasColumnType("datetime(6)");
-            entity.Property(e => e.ModifiedAt).HasColumnOrder(15).HasColumnName("modified_at").HasColumnType("datetime(6)");
+            entity.Property(e => e.IsDeleted).HasColumnOrder(6).HasColumnName("is_deleted").HasColumnType("tinyint(1)");
+            entity.Property(e => e.CreatedAt).HasColumnOrder(7).HasColumnName("created_at").HasColumnType("datetime(6)");
+            entity.Property(e => e.ModifiedAt).HasColumnOrder(8).HasColumnName("modified_at").HasColumnType("datetime(6)");
         });
         
         modelBuilder.Entity<Employee>(entity =>
@@ -55,14 +55,15 @@ class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
             entity.HasKey(pk => pk.Id);
             entity.HasOne(np => np.Person).WithOne().HasForeignKey<Employee>(fk => fk.PersonId);
 
-            entity.Property(e => e.Id).HasColumnOrder(1).HasColumnName("id").HasColumnType("int");
-            entity.Property(e => e.PersonId).HasColumnOrder(2).HasColumnName("codice_fiscale").HasColumnType("varchar(16)");
+            entity.Property(e => e.Id).HasColumnOrder(1).HasColumnName("id").HasColumnType("int").ValueGeneratedOnAdd().IsRequired();
+            entity.Property(e => e.PersonId).HasColumnOrder(2).HasColumnName("person_id").HasColumnType("int");
             entity.Property(e => e.CodiceMeccanografico).HasColumnOrder(3).HasColumnName("codice_meccanografico").HasColumnType("varchar(10)");
-            entity.Property(e => e.Ruolo).HasColumnOrder(4).HasColumnName("ruolo").HasColumnType("varchar(50)");
-            entity.Property(e => e.Salario).HasColumnOrder(5).HasColumnName("salario").HasColumnType("decimal(18,2)");
-            entity.Property(e => e.IsDeleted).HasColumnOrder(13).HasColumnName("is_deleted").HasColumnType("tinyint(1)");
-            entity.Property(e => e.CreatedAt).HasColumnOrder(14).HasColumnName("created_at").HasColumnType("datetime(6)");
-            entity.Property(e => e.ModifiedAt).HasColumnOrder(15).HasColumnName("modified_at").HasColumnType("datetime(6)");
+            entity.Property(e => e.Password).HasColumnOrder(4).HasColumnName("password").HasColumnType("varchar(24)");
+            entity.Property(e => e.Ruolo).HasColumnOrder(5).HasColumnName("ruolo").HasColumnType("varchar(50)");
+            entity.Property(e => e.Salario).HasColumnOrder(6).HasColumnName("salario").HasColumnType("decimal(18,2)");
+            entity.Property(e => e.IsDeleted).HasColumnOrder(7).HasColumnName("is_deleted").HasColumnType("tinyint(1)");
+            entity.Property(e => e.CreatedAt).HasColumnOrder(8).HasColumnName("created_at").HasColumnType("datetime(6)");
+            entity.Property(e => e.ModifiedAt).HasColumnOrder(9).HasColumnName("modified_at").HasColumnType("datetime(6)");
         });
     }
 }
--- _prjFinale/system.core/_/Domain/Entity/User/Employee.cs
+++ _prjFinale/system.core/_/Domain/Entity/User/Employee.cs
@@ -4,6 +4,7 @@ public class Employee : BaseEntity
 {
     public int PersonId { get; set; }
     public string CodiceMeccanografico { get; set; } = default!;
+    public string Password { get; set; } = default!;
     public string Ruolo { get; set; } = default!;
     public decimal Salario { get; set; }
 
@@ -11,10 +12,11 @@ public class Employee : BaseEntity
     public Person? Person { get; set; }
 
     public Employee(int id, bool isDeleted, DateTime createdAt, DateTime modifiedAt, int personId, 
-        string codiceMeccanografico, string ruolo, decimal salario) : base(id, isDeleted, createdAt, modifiedAt) 
+        string codiceMeccanografico, string password, string ruolo, decimal salario) : base(id, isDeleted, createdAt, modifiedAt) 
     { 
         PersonId = personId; 
         CodiceMeccanografico = codiceMeccanografico;
+        Password = password;
         Ruolo = ruolo;
         Salario = salario;
     }
